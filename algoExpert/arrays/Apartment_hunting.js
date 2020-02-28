30f671787e91697e4d737fe0fcbfa7c33476a0e7
ashap94 <aaronshapiro94@gmail.com>
2020-02-27 18:00:03 -0800

    Arrays - Apartment Hunting, Algo Expert
    Implemented better solution
    Time O(BR)   ||   Space O(BR)

--- algoExpert/arrays/Apartment_hunting.js
+++ algoExpert/arrays/Apartment_hunting.js
@@ -42,3 +42,67 @@ function getMinDistance(array) {
 function distanceBetween(a, b) {
   return Math.abs(a - b);
 }
+
+// second solution,
+// Time O(BR) || Space O(BR)
+
+function apartmentHunting(blocks, reqs) {
+  // Write your code here.
+  const minDistancesFromBlocks = reqs.map(req =>
+    getMinDistancesFromBlocks(req, blocks)
+  );
+  const maxDistancesAtBlocks = getMaxDistancesAtBlocks(
+    blocks,
+    minDistancesFromBlocks
+  );
+  return getIdxAtMinValue(maxDistancesAtBlocks);
+}
+
+function getMinDistancesFromBlocks(req, blocks) {
+  const minDistances = new Array(blocks.length);
+  let closestDistance = Infinity;
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks[i][req]) closestDistance = i;
+    minDistances[i] = getMinDistance(i, closestDistance);
+  }
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    if (blocks[i][req]) closestDistance = i;
+    minDistances[i] = Math.min(
+      minDistances[i],
+      getMinDistance(i, closestDistance)
+    );
+  }
+
+  return minDistances;
+}
+
+function getMaxDistancesAtBlocks(blocks, minDistancesFromBlocks) {
+  let maxDistancesAtBlocks = new Array(blocks.length);
+
+  for (let i = 0; i < blocks.length; i++) {
+    const distancesFromReqs = minDistancesFromBlocks.map(
+      distances => distances[i]
+    );
+    maxDistancesAtBlocks[i] = Math.max(...distancesFromReqs);
+  }
+
+  return maxDistancesAtBlocks;
+}
+
+function getIdxAtMinValue(array) {
+  let minValue = array[0];
+  let minIdx = 0;
+  for (let i = 0; i < array.length; i++) {
+    let currentValue = array[i];
+    if (currentValue < minValue) {
+      minValue = currentValue;
+      minIdx = i;
+    }
+  }
+
+  return minIdx;
+}
+
+function getMinDistance(a, b) {
+  return Math.abs(a - b);
+}
