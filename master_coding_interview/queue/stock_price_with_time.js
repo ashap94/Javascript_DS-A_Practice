09e6e4204056b1ba0edd8be1d49a6acad4743023
ashap94 <aaronshapiro94@gmail.com>
2020-06-16 12:58:36 -0700

    Stock Price with Time - Queue InterviewCamp.io

--- master_coding_interview/queue/stock_price_with_time.js
+++ master_coding_interview/queue/stock_price_with_time.js
@@ -9,9 +9,43 @@ class Price {
 
 class StockPriceWithTime {
   constructor(window) {
-    this.first = null;
-    this.last = null;
     this.window = window;
     this.queue = new Queue();
   }
+
+  add(price, day) {
+    while (
+      !this.queue.isEmpty() &&
+      this.queue.peek().value.day < day - this.window + 1
+    ) {
+      this.queue.dequeue();
+    }
+    this.queue.enqueue(new Price(price, day));
+  }
+
+  getMax() {
+    return this.iterate();
+  }
+
+  iterate() {
+    let currentNode = this.queue.first;
+    let max = null;
+    while (currentNode !== null) {
+      console.log("ITERATE", currentNode.value);
+      max = Math.max(currentNode.value.price, max);
+      currentNode = currentNode.next;
+    }
+    return max ? max : null;
+  }
 }
+
+const stocks = new StockPriceWithTime(3);
+stocks.add(32, 1);
+stocks.add(45, 1);
+stocks.add(37, 2);
+stocks.add(42, 3);
+// stocks.add(40, 4);
+
+console.log("HERE'S MAX", stocks.getMax());
+
+console.log(stocks.queue);
